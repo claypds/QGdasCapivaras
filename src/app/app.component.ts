f70534573f88e9cf695da09387eecb3704024ad2
Tiago Vidal de Souza <tirvs87@gmail.com>
2018-10-23 14:12:16 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'qgc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'WebApp';
+}
